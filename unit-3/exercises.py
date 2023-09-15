51a33e1c2202ca7406fda309376e2910765abd3a
d805696 <rpioli@sf.prefeitura.sp.gov.br>
2023-09-15 19:39:33 -0300

    Unit commit

---
+++ unit-3/exercises.py
@@ -0,0 +1,9 @@
+# display today´s date
+from datetime import date
+
+print(date.today())
+
+# build a unit converter
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + ' parsecs is ' + str(lightyears) + ' lightyears')
