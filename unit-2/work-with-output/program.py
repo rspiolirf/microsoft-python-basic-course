4052b3429000c018b2dca597b4b65e6d0b032e70
d805696 <rpioli@sf.prefeitura.sp.gov.br>
2023-09-15 19:21:46 -0300

    Unit 1 and Unit 2 commit

---
+++ unit-2/work-with-output/program.py
@@ -0,0 +1,23 @@
+sum = 1 + 2
+print(sum)
+
+print("show this in the console")
+
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #string
+
+print(type(distance_to_alpha_centauri))
+
+left_side = 10
+right_side = 5
+print(left_side / right_side) # 2
+
+from datetime import date
+print(date.today())
+print("Today´s date is: " + str(date.today()))
