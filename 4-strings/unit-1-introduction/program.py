056d3eacdf41128520e876dbc9d21dfc5c234e51
d805696 <rpioli@sf.prefeitura.sp.gov.br>
2023-09-17 21:29:26 -0300

    Finish Module 3 Boolean Logic

---
+++ 4-strings/unit-1-introduction/program.py
@@ -0,0 +1,6 @@
+# What will you learn?
+# By the end of this module, you'll be able to:
+
+# Use special string methods.
+# Use variables to introduce values in text.
+# Apply other advanced formatting techniques to change how text is presented.
